11c56882987a598db4bf66b40c02d7b5f07409a5
Jhean Carllos <jheanrabelo01@gmail.com>
2025-06-22 16:34:13 -0300

    CRIANDO MODELO LIMIT

--- src/main/kotlin/br/com/pessoal/limit/model/LimitModel.kt
+++ src/main/kotlin/br/com/pessoal/limit/model/LimitModel.kt
@@ -5,29 +5,35 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import java.time.LocalDateTime
 
-@Entity(name = "customer")
+@Entity(name = "`limit`")
 data class LimitModel(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY )
     var id: Int? = null,
 
     @Column(name = "owner_id")
-    var ownerId: Long
+    var ownerId: Long,
+
+    @Column
+    var period: String,
+
+    @Column(name = "current_amout")
+    var currentAmount: Long,
+
+    @Column(name = "max_amount")
+    var maxAmount: Long,
+
+    @Column(name = "limit_category")
+    var limitCategory: String,
+
+    @Column(name = "created_at")
+    var createdAt: LocalDateTime,
+
+    @Column(name = "updated_at")
+    var updatedAt: LocalDateTime
+
 
 )
 
-//@Entity(name = "customer")
-//data class CustomerModel (
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY )
-//    var id: Int? = null,
-//
-//    @Column
-//    var name: String,
-//
-//    @Column
-//    var email: String
-//
-//)
